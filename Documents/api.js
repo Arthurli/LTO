3fc09059c779a6d0cdda0287b352db5abf33130f
Arthur <arthurli1991@sina.com>
2015-08-07 13:54:44 +0800

    Add api doc

---
+++ Documents/api.js
@@ -0,0 +1,120 @@
+/**
+ * @api {post} /init 获取token
+ * @apiName init
+ * @apiDescription 通过获取token 才能进行其他api请求
+ * @apiGroup Init
+ */
+
+ /**
+ * @api {get} /users/:uuid 获取用户信息
+ * @apiName get user
+ * @apiGroup User
+ */
+
+ /**
+ * @api {delete} /users/:uuid 删除用户
+ * @apiName delete user
+ * @apiGroup User
+ */
+
+ /**
+ * @api {put} /users/:uuid 修改用户信息
+ * @apiName update user
+ * @apiGroup User
+ */
+
+ /**
+ * @api {post} /users 创建新用户
+ * @apiName create user
+ * @apiGroup User
+ */
+
+ /**
+ * @api {get} /labels 获取label列表
+ * @apiName label list
+ * @apiGroup Label
+ */
+
+ /**
+ * @api {get} /labels/:uuid 获取特定label
+ * @apiName get label
+ * @apiGroup Label
+ */
+
+ /**
+ * @api {post} /labels 创建label
+ * @apiName create label
+ * @apiGroup Label
+ */
+
+/**
+ * @api {delete} /labels/:uuid 删除label
+ * @apiName delete label
+ * @apiGroup Label
+ */
+
+ /**
+ * @api {put} /labels/:uuid 更新label
+ * @apiName update label
+ * @apiGroup Label
+ */
+
+ /**
+ * @api {get} /todos 获取任务列表
+ * @apiName todo list
+ * @apiGroup Todo
+ */
+
+/**
+ * @api {get} /todos/:uuid 获取特定任务
+ * @apiName get todo
+ * @apiGroup Todo
+ */
+
+ /**
+ * @api {delete} /todos/:uuid 删除任务
+ * @apiName delete todo
+ * @apiGroup Todo
+ */
+
+ /**
+ * @api {put} /todos/:uuid 更新任务
+ * @apiName 刷新任务
+ * @apiGroup Todo
+ */
+
+/**
+ * @api {get} /groups 获取用户群组列表
+ * @apiName group list
+ * @apiGroup Group
+ */
+
+/**
+ * @api {get} /groups/:uuid 获取群组信息
+ * @apiName get group
+ * @apiGroup Group
+ */
+
+ /**
+ * @api {delete} /groups/:uuid 删除群组
+ * @apiName delete group
+ * @apiGroup Group
+ */
+
+/**
+ * @api {put} /groups/:uuid 更新群组
+ * @apiName update group
+ * @apiGroup Group
+ */
+
+ /**
+ * @api {get} /todos/:uuid/comments 获取任务评论
+ * @apiName comments list
+ * @apiGroup Comments
+ */
+
+/**
+ * @api {post} /todos/:uuid/comments 添加新评论
+ * @apiName Add comments
+ * @apiGroup Comments
+ */
